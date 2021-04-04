ffd010c17a1e95c984a969f042b00f0aa6471641
NolanFlegel <flegel2n@uregina.ca>
2021-04-03 23:01:17 -0700

    converted postalcode to use regex

--- RiskMeter/src/main/java/PostalCode.java
+++ RiskMeter/src/main/java/PostalCode.java
@@ -16,54 +16,59 @@
 public class PostalCode 
 {
 	private String postalCode;
-
-    	public static boolean isValidPostalCode(String postalCode)
+	private String regexPostal = "^[Kk][1][A-Ta-t][-| ][0-9][A-Za-z][0-9]$";
+    	public boolean isValidPostalCode(String postalCode)
     	{
+			
+
+			if (!postalCode.matches(this.regexPostal)){
+				return false;
+			}
 			//ToDo: add you code here
-			int length = postalCode.length();
-			String prefix = postalCode.substring(0, 2);	// expects to be "K1"
+			// int length = postalCode.length();
+			// String prefix = postalCode.substring(0, 2);	// expects to be "K1"
 
-			// Capitalize the given vertical index
-			char verticalIndex = Character.toUpperCase(postalCode.charAt(2));
+			// // Capitalize the given vertical index
+			// char verticalIndex = Character.toUpperCase(postalCode.charAt(2));
 		
-			String temp = postalCode.substring(0, 2) + verticalIndex + postalCode.substring(3);
-			postalCode = temp;
+			// String temp = postalCode.substring(0, 2) + verticalIndex + postalCode.substring(3);
+			// postalCode = temp;
 
-			// Spacing character
-			char spacing = postalCode.charAt(3);
+			// // Spacing character
+			// char spacing = postalCode.charAt(3);
 
-			char horizontalIndex = postalCode.charAt(4);
+			// char horizontalIndex = postalCode.charAt(4);
 
-			if (length != 7)
-			{
-				return false;
-			}
-			else if (prefix.equals("K1") == false)
-			{
-				return false;
-			}
-			else if (verticalIndex == 'U' | verticalIndex == 'V' | verticalIndex == 'W' | verticalIndex == 'X' | 
-			verticalIndex == 'Y' | verticalIndex == 'Z')
-			{
-				return false;
-			}
-			else if (!(spacing == '-' | spacing == ' '))
-			{
-				return false;
-			}
-			else if (!(horizontalIndex == '0' | horizontalIndex == '1' | horizontalIndex == '2' | horizontalIndex == '3' | horizontalIndex == '4'
-			| horizontalIndex == '5' | horizontalIndex == '6' | horizontalIndex == '7' | horizontalIndex == '8' | horizontalIndex == '9'))
-			{
-				return false;
-			}
-			else if (Character.isDigit(postalCode.charAt(5)))
-			{
-				return false;
-			}
-			else if (!Character.isDigit(postalCode.charAt(6)))
-			{
-				return false;
-			}
+			// if (length != 7)
+			// {
+			// 	return false;
+			// }
+			// else if (prefix.equals("K1") == false)
+			// {
+			// 	return false;
+			// }
+			// else if (verticalIndex == 'U' | verticalIndex == 'V' | verticalIndex == 'W' | verticalIndex == 'X' | 
+			// verticalIndex == 'Y' | verticalIndex == 'Z')
+			// {
+			// 	return false;
+			// }
+			// else if (!(spacing == '-' | spacing == ' '))
+			// {
+			// 	return false;
+			// }
+			// else if (!(horizontalIndex == '0' | horizontalIndex == '1' | horizontalIndex == '2' | horizontalIndex == '3' | horizontalIndex == '4'
+			// | horizontalIndex == '5' | horizontalIndex == '6' | horizontalIndex == '7' | horizontalIndex == '8' | horizontalIndex == '9'))
+			// {
+			// 	return false;
+			// }
+			// else if (Character.isDigit(postalCode.charAt(5)))
+			// {
+			// 	return false;
+			// }
+			// else if (!Character.isDigit(postalCode.charAt(6)))
+			// {
+			// 	return false;
+			// }
 			
 			return true;
 		}
