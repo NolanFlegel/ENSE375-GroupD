a8a1048ba1a6b2ffac48160b30b363ee52032422
Nolan Flegel <flegel2n@uregina.ca>
2021-04-05 21:01:26 -0700

    Update TestDuckDuckGo.java

--- SeleniumTesting/src/test/java/TestDuckDuckGo.java
+++ SeleniumTesting/src/test/java/TestDuckDuckGo.java
@@ -5,20 +5,38 @@
 //Testing DuckDuckGo in Firefox and Chrome
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestDuckDuckGo{
-    private WebDriver driver;
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void setupTest() throws MalformedURLException {
+        //Desired Capabilities
+        DesiredCapabilities capability = new DesiredCapabilities();
+        capability.setCapability("browser", "chrome");
+       
+        driver = new RemoteWebDriver(new URL("http://34.201.70.191:4444/wd/hub"), capability);
+    }
 
     @Test
     public void getTitleTest(){
-        driver = new ChromeDriver();
+        // DesiredCapabilities capability = new DesiredCapabilities();
+        // capability.setCapability("browser", "Chrome");
+        //driver = new RemoteWebDriver(new URL("http://34.201.70.191:4445/wd/hub"), capability);
         driver.get("https://duckduckgo.com/");
         assertTrue(driver.getTitle().contains("DuckDuckGo"));
         driver.quit();
@@ -26,7 +44,9 @@ public class TestDuckDuckGo{
 
     @Test
     public void getSearchResult(){
-        driver = new ChromeDriver();
+        // DesiredCapabilities capability = new DesiredCapabilities();
+        // capability.setCapability("browser", "Chrome");
+       // driver = new RemoteWebDriver(new URL("http://34.201.70.191:4445/wd/hub"), capability);
         driver.get("https://duckduckgo.com/");
         WebElement element = driver.findElement(By.name("q"));
         element.sendKeys("uregina");
